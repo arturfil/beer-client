2ffdd534531f795d78110a81d05971fed33eea9e
Arturo Filio Villa <arturfil@hotmail.com>
2017-11-09 23:16:49 -0500

    finished login component html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { BeerListComponent } from './pages/beer-list/beer-list.component';
+import { LoginComponent } from './pages/login/login.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 const routes: Routes = [
-    { path: '',         component: BeerListComponent}
+    { path: '',         component: BeerListComponent},
+    { path: 'login',    component: LoginComponent   },
+    { path: 'signup',   component: SignupComponent  }
 ];
 
 @NgModule({
