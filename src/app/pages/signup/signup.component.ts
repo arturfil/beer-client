b17c1ff9f66ed0612220fcd98c8af441c5b6cbab
Arturo Filio Villa <arturfil@hotmail.com>
2017-11-09 23:59:09 -0500

    changed nav bar and finished signup setup

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { SignupInfo } from '../../interfaces/signup-info';
+import { LoginInfo } from '../../interfaces/login-info';
+import { AuthApiService } from '../../services/auth-api.service';
 
 @Component({
   selector: 'app-signup',
@@ -7,9 +12,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 
-  constructor() { }
+  newUser: SignupInfo = {
+    signupFullName: '',
+    signupUsername: '',
+    signupPassword: ''
+  }
+
+  loginUser: LoginInfo = {
+    loginUsername: '',
+    loginPassword: ''
+  }
+
+  errorMessage: string;
+  loginError: string;
+
+  constructor(
+    private authThang: AuthApiService,
+    private routerThang: Router
+  ) { }
 
   ngOnInit() {
+  }
+
+  signupSubmit() {
+    this.authThang.postSignup(this.newUser)
+      .subscribe(
+        (userInfo) => {
+          this.routerThang.navigate(['']);
+        },
+        (errInfo) => {
+          console.log('Sign up error', errInfo);
+          if (errInfo.status === 400) {
+            this.errorMessage = "Validation error";
+          } else {
+            this.errorMessage = "Something went wrong. Try again later";
+          }
+        }
+      )
+  }
+
+  loginSubmit() {
+    this.authThang.postLogin(this.loginUser)
+      .subscribe(
+        (userInfo) => {
+          this.routerThang.navigate(['']);
+        },
+        (errInfo) => {
+          console.log('Log in error', errInfo);
+          if (errInfo.status === 401) {
+            this.loginError = "Bad Credentials";
+          } else {
+            this.loginError = "Something went wrong, try again later"
+          }
+        }
+      )
   }
 
 }
