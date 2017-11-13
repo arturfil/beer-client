e7492ff8676b2dbdb06fc350c2f68405e74a61ba
Arturo Filio Villa <arturfil@hotmail.com>
2017-11-13 11:56:37 -0500

    settup of home page component

--- src/app/pages/beer-list/beer-list.component.ts
+++ src/app/pages/beer-list/beer-list.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+import { BeerApiService } from '../../services/beer-api.service';
+import { AuthApiService } from '../../services/auth-api.service';
+
+import { environment } from '../../../environments/environment';
+
 @Component({
   selector: 'app-beer-list',
   templateUrl: './beer-list.component.html',
@@ -7,9 +12,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeerListComponent implements OnInit {
 
-  constructor() { }
+  imageDomain = environment.apiUrl
+
+  beers: any[] = [];
+  userInfo: any;
+  
+  constructor(
+    private beerThang: BeerApiService,
+    private authThang: AuthApiService,
+  ) { }
 
   ngOnInit() {
+    this.beerThang.getBeers()
+      .subscribe(
+        (beersFromApi: any[]) => {
+          this.beers = beersFromApi;
+        }
+      );
+
+      this.authThang.getLoginStatus()
+        .subscribe(
+          (loggedInInfo: any) => {
+            if (loggedInInfo.isLoggedIn) {
+              this.userInfo = loggedInInfo.userInfo;
+            }
+          }
+        );
+  }
+
+  handleNewBeer(theBeer) {
+    this.beers.unshift(theBeer);
   }
 
 }
--- src/app/services/beer-api.service.ts
+++ src/app/services/beer-api.service.ts
@@ -1,8 +1,55 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { BeerInfo } from '../interfaces/beer-info';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class BeerApiService {
 
-  constructor() { }
+  baseUrl: string = environment.apiUrl;
+
+  constructor(
+    private httpThang: HttpClient
+  ) { }
+
+  // GET/api/beers
+  getBeers() {
+    return this.httpThang.get(
+      this.baseUrl + '/api/items'
+    );
+  }
+
+  // GET/api/beers/ID
+  getBeerDetails(beerId: string) {
+    return this.httpThang.get(
+      this.baseUrl + '/api/beers/' + beerId
+    );
+  }
+
+  // POST/api/beers
+  postBeer(beerFields: BeerInfo) {
+    return this.httpThang.post(
+      this.baseUrl + '/api/beers/',
+      beerFields,
+      { withCredentials: true }
+    );
+  }
+
+  // DELETE/api/items/ID
+  deleteItem(itemId: string) { 
+    return this.httpThang.delete(
+      this.baseUrl + '/api/items/' + itemId,
+      { withCredentials: true }
+    );
+  }
+
+  // GET/api/myitems
+  getMyItems() {
+    return this.httpThang.get(
+      this.baseUrl + '/api/myitems',
+      { withCredentials: true }
+    );
+  }
 
 }
