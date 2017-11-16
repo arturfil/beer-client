053c4323ead2fc3e69ab70cb3bb7c03e3d2dcd35
Arturo Filio Villa <arturfil@hotmail.com>
2017-11-16 00:54:56 -0500

    added css and logic to the beer.details component

--- src/app/pages/beer-details/beer-details.component.ts
+++ src/app/pages/beer-details/beer-details.component.ts
@@ -12,7 +12,7 @@ import { environment } from '../../../environments/environment';
 })
 export class BeerDetailsComponent implements OnInit {
 
-  beerInfo: any;
+  beerInfo: any = {};
   userInfo: any;
 
   constructor(
